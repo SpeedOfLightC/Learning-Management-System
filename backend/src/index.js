121a67bcafb9af728f76994dee61358a9fb2bab1
SpeedOfLight03 <arnab.ghosh.it2021@kgec.ac.in>
2024-09-07 00:59:38 +0530

    chore: Add initial project files and configurations

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+import { app } from './app.js';
+
+
+dotenv.config({
+    path: './env'
+});
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`App is running on port ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log(`App connection failed !! ${error}`);
+})
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from 'mongoose';
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        default: "user",
+        enum: ["user", "admin"],
+    },
+    subscription: [{
+        type: Schema.Types.ObjectId,
+        ref: "Course"
+    }]
+},
+{
+    timestamps: true,
+});
+
+export const User = mongoose.model("User", userSchema);
