b33951452099aa5df9abda98147ca5375e2ece14
SpeedOfLight03 <arnab.ghosh.it2021@kgec.ac.in>
2024-09-08 01:35:51 +0530

    chore: Add nodemailer dependency and implement email verification

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,9 +1,72 @@
+import { User } from '../models/user.model.js';
 import { asyncHandler } from '../utils/asyncHandler.js'
+import { ApiError } from '../utils/ApiError.js';
+import { ApiResponse } from '../utils/ApiResponse.js';
+import sendMail from '../middlewares/mail.middleware.js';
 
-const registerUser = asyncHandler(async(req, res) => {
+const options = {
+    httpOnly: true,
+    secure: true
+}
+
+const registerUser = asyncHandler(async (req, res) => {
+    console.log("here");
     
+    const { email, name, password } = req.body;
+
+    const user = await User.findOne({
+        $or: [{ email }]
+    })
+
+    if (user) {
+        return new ApiError(400, "User already exists");
+    }
+
+    const otp = Math.floor(Math.random() * 1000000);
+    // const accessToken = user.generateAccessToken();
+
+    const data = {
+        name,
+        otp
+    }
+
+    await sendMail(
+        email,
+        "LMS OTP Verification",
+        data
+    );    
+
+    res.status(200)
+        .json(
+            {
+                message: "OTP sent to your mail",
+                // accessToken
+            }
+        );
+
+    // const newUser = await User.create({
+    //     email,
+    //     name,
+    //     password
+    // })
+
+    // const createdUser = await User.findById(newUser._id).select("-password");
+
+    // if (!createdUser) {
+    //     return new ApiError(500, "User not created");
+    // }
+
+    // return res.status(201)
+    //     .cookie("accessToken", accessToken, options)
+    //     .json(
+    //         new ApiResponse(200,
+    //             {
+    //                 user: createdUser,
+    //                 accessToken
+    //             }, "User created successfully")
+    //     )
 });
 
-export{
+export {
     registerUser
 }
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
+import bcrypt, { genSalt } from 'bcrypt';
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema({
     name: {
@@ -24,8 +26,35 @@ const userSchema = new Schema({
         ref: "Course"
     }]
 },
-{
-    timestamps: true,
-});
+    {
+        timestamps: true,
+    });
 
 export const User = mongoose.model("User", userSchema);
+
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    const salt = await genSalt();
+
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email
+        },
+        process.env.ACESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACESS_TOKEN_EXPIRY
+        }
+    )
+}
